885ba3f2e6330dc22b7ad6c458d691a7d8711c29
Robert Böhnke <robb@robb.is>
2012-06-30 17:53:41 +0200

    Adding front-end oEmbed solution

---
+++ js/oEmbed.coffee
@@ -0,0 +1,74 @@
+oEmbed = window.oEmbed ?= {}
+
+# A list of oEmbed providers and their respective endpoints
+oEmbed.endpoints =
+  'soundcloud.com' : 'https://soundcloud.com/oembed.json'
+  'vimeo.com'      : 'https://vimeo.com/api/oembed.json'
+  'youtube.com'    : 'http://api.embed.ly/1/oembed'
+
+oEmbed.errorMessage = 'Failed to embed'
+
+oEmbed.config = {}
+
+oEmbed.parametersForNode = (node) ->
+  attributes = {}
+
+  for {name, value} in node.attributes
+    if match = name.match /^data-(.+)/
+      attributes[match[1]] = value
+
+  attributes
+
+oEmbed.embed = (embed) ->
+  attributes = @parametersForNode embed
+
+  # The host of this URL, including subdomains
+  host = attributes.url.match(/https?\:\/\/([^\/]+)(\/.+)?/)[1]
+
+  # The first provider matching this host
+  provider = (provider for provider of @endpoints when host.match provider)[0]
+
+  parameters = []
+  parameters.push "#{k}=#{encodeURIComponent v}" for k, v of attributes
+  parameters.push "#{k}=#{encodeURIComponent v}" for k, v of oEmbed.config[provider]
+
+  # Create a request URL from the provider's oEmbed endpoint
+  requestURL =  oEmbed.endpoints[provider]
+  requestURL += '?' + parameters[0] + '&' + parameters[1..].join('&')
+
+  success = (result) ->
+    container = document.createElement 'div'
+    container.classList.add 'embed'
+    container.classList.add result?.type
+    container.classList.add result?.provider_name.toLowerCase()
+
+    container.innerHTML = result.html
+
+    embed.parentNode.replaceChild container, embed
+
+  error = ->
+    container = document.createElement 'div'
+    container.classList.add 'embed'
+    container.classList.add 'failed'
+
+    container.innerHTML = oEmbed.errorMessage
+
+    embed.parentNode.replaceChild container, embed
+
+  oEmbed.fetchJSON requestURL, {success, error}
+
+oEmbed.fetchJSON = (url, {success, error}) ->
+  xmlHTTP = XMLHttpRequest and new XMLHttpRequest() or
+            new ActiveXObject 'Microsoft.XMLHTTP'
+
+  xmlHTTP.onload = ->
+    json = JSON.parse xmlHTTP.responseText
+    success? json
+
+  xmlHTTP.onerror = ->
+    error?()
+
+  xmlHTTP.addEventListener 'error', error, no
+
+  xmlHTTP.open 'GET', url, yes
+  xmlHTTP.send()
---
+++ js/oEmbed.js
@@ -0,0 +1,91 @@
+(function() {
+  var oEmbed, _ref;
+
+  oEmbed = (_ref = window.oEmbed) != null ? _ref : window.oEmbed = {};
+
+  oEmbed.endpoints = {
+    'soundcloud.com': 'https://soundcloud.com/oembed.json',
+    'vimeo.com': 'https://vimeo.com/api/oembed.json',
+    'youtube.com': 'http://api.embed.ly/1/oembed'
+  };
+
+  oEmbed.errorMessage = 'Failed to embed';
+
+  oEmbed.config = {};
+
+  oEmbed.parametersForNode = function(node) {
+    var attributes, match, name, value, _i, _len, _ref2, _ref3;
+    attributes = {};
+    _ref2 = node.attributes;
+    for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+      _ref3 = _ref2[_i], name = _ref3.name, value = _ref3.value;
+      if (match = name.match(/^data-(.+)/)) attributes[match[1]] = value;
+    }
+    return attributes;
+  };
+
+  oEmbed.embed = function(embed) {
+    var attributes, error, host, k, parameters, provider, requestURL, success, v, _ref2;
+    attributes = this.parametersForNode(embed);
+    host = attributes.url.match(/https?\:\/\/([^\/]+)(\/.+)?/)[1];
+    provider = ((function() {
+      var _results;
+      _results = [];
+      for (provider in this.endpoints) {
+        if (host.match(provider)) _results.push(provider);
+      }
+      return _results;
+    }).call(this))[0];
+    parameters = [];
+    for (k in attributes) {
+      v = attributes[k];
+      parameters.push("" + k + "=" + (encodeURIComponent(v)));
+    }
+    _ref2 = oEmbed.config[provider];
+    for (k in _ref2) {
+      v = _ref2[k];
+      parameters.push("" + k + "=" + (encodeURIComponent(v)));
+    }
+    requestURL = oEmbed.endpoints[provider];
+    requestURL += '?' + parameters[0] + '&' + parameters.slice(1).join('&');
+    success = function(result) {
+      var container;
+      container = document.createElement('div');
+      container.classList.add('embed');
+      container.classList.add(result != null ? result.type : void 0);
+      container.classList.add(result != null ? result.provider_name.toLowerCase() : void 0);
+      container.innerHTML = result.html;
+      return embed.parentNode.replaceChild(container, embed);
+    };
+    error = function() {
+      var container;
+      container = document.createElement('div');
+      container.classList.add('embed');
+      container.classList.add('failed');
+      container.innerHTML = oEmbed.errorMessage;
+      return embed.parentNode.replaceChild(container, embed);
+    };
+    return oEmbed.fetchJSON(requestURL, {
+      success: success,
+      error: error
+    });
+  };
+
+  oEmbed.fetchJSON = function(url, _arg) {
+    var error, success, xmlHTTP;
+    success = _arg.success, error = _arg.error;
+    xmlHTTP = XMLHttpRequest && new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
+    xmlHTTP.onload = function() {
+      var json;
+      json = JSON.parse(xmlHTTP.responseText);
+      return typeof success === "function" ? success(json) : void 0;
+    };
+    xmlHTTP.onerror = function() {
+      return typeof error === "function" ? error() : void 0;
+    };
+    xmlHTTP.addEventListener('error', error, false);
+    xmlHTTP.open('GET', url, true);
+    return xmlHTTP.send();
+  };
+
+}).call(this);
