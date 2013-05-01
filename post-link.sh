758b03665e997d5405451d8c8bdf87f01dd0e580
Robert Böhnke <robb@robb.is>
2013-05-01 23:51:37 +0200

    - instead of _

--- post-link.sh
+++ post-link.sh
@@ -2,7 +2,7 @@ date=$(date +%F)
 
 title=$( curl -s $1 | awk 'BEGIN{IGNORECASE=1;FS="<title>|</title>";RS=EOF} {print $2}' | tr -d "\n")
 
-filename="$date-$( echo $title | tr "A-Z" "a-z" | tr " " "_" ).md"
+filename="$date-$( echo $title | tr "A-Z" "a-z" | tr " " "-" ).md"
 
 post=$(cat <<POST
 ---
